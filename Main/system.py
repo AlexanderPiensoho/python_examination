7958d05e8224e799a8a475c5ff4e01648c491ee2
piensoho@gmail.com <piensoho@gmail.com>
2025-10-01 09:04:58 +0200

    first commit, program dosnt work correctly yet.

--- Main/system.py
+++ Main/system.py
@@ -1,14 +1,16 @@
 from monitor import start_monitoring, show_monitoring
 import psutil
+import time
 alarms = {
-    "cpu": None,
-    "memory": None,
-    "disk": None
+    "cpu": [],
+    "memory": [],
+    "disk": []
 }
 
 main_menu_is_running = True
 set_alarm_menu_is_running = False
 monitoring_active = False
+alarm_monitoring = False
 
 print("==Välkommen till huvudmenyn==".upper())
 
@@ -47,29 +49,56 @@ while main_menu_is_running:
 
         if alarm_menu_choice == "1":
             cpu_alarm = int(input("Ställ in en nivå för CPU alarm, mellan 0-100%: "))
-            alarms["cpu"] = cpu_alarm
+            alarms["cpu"].append(cpu_alarm)
             print(f"Ditt CPU alarm är inställt på {cpu_alarm}%\n")
         elif alarm_menu_choice == "2":
             memory_alarm = int(input("Ställ in en nivå för ditt RAM-minne alarm, mellan 0-100%: "))
-            alarms["memory"] = memory_alarm
+            alarms["memory"].append(memory_alarm)
             print(f"Ditt RAM-minne alarm är inställt på {memory_alarm}%\n")
         elif alarm_menu_choice == "3":
             disk_alarm = int(input("Ställ in en nivå för ditt Disk-minne alarm, mellan 0-100%: "))
-            alarms["disk"] = disk_alarm
+            alarms["disk"].append(disk_alarm)
             print(f"Ditt disk-minne alarm är inställt på {disk_alarm}%\n")
         elif alarm_menu_choice == "4":
             print("\nDu skickas tillbaka till huvudmenyn\n".upper())
             set_alarm_menu_is_running = False
         else:
             print("Du måste välja en siffra mellan 1-4")
+      
 
-    elif menu_choice == "4":
-        print(f"\nCPU alarm: {cpu_alarm}%".upper())
-        print(f"RAM-minne alarm: {memory_alarm}%".upper())
-        print(f"Disk-minne alarm: {cpu_alarm}%\n".upper())
+    elif menu_choice == "4":  
+        print(f"\nCPU alarm: {alarms["cpu"]}%".upper())
+        print(f"RAM-minne alarm: {alarms["memory"]}%".upper())
+        print(f"Disk-minne alarm: {alarms["disk"]}%\n".upper())
         input("Tryck enter för att komma tillbaka till menyn...")
     elif menu_choice == "5":
-        pass
+        alarm_monitoring = True
+        while alarm_monitoring:
+            print("Aktiv övervakning pågår")
+            time.sleep(0.5)
+            for alarm in alarms["cpu"]:
+                if int(psutil.cpu_percent(interval=1)) >= int(alarm):
+                    print("Varning")
+            for alarm in alarms["memory"]:
+                if int(psutil.virtual_memory()) >= int(alarm):
+                    print("Varning")
+            for alarm in alarms["disk"]:
+                if int(psutil.disk_usage()) >= int(alarm):
+                    print("Varning")
+
+            '''
+            if alarms["cpu"] == []:
+                print("VARNING!! CPU")
+                time.sleep(1)
+            elif alarms["memory"] == []:
+                print("VARNING!! RAM-MINNE")
+                time.sleep(1)
+            elif alarms["disk"] == []:
+                print("VARNING!! DISK-MINNE")
+                time.sleep(1)
+            else:
+                continue
+                '''
     elif menu_choice == "6":
         pass
     elif menu_choice == "7":
