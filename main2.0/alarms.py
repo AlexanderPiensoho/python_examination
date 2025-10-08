38279615fa7a4835493cdb2a22dd083f3dd022da
piensoho@gmail.com <piensoho@gmail.com>
2025-10-08 17:29:04 +0200

    moved back prod to 1.0

--- main2.0/alarms.py
+++ main2.0/alarms.py
@@ -1,10 +1,23 @@
 import psutil
+import json
+#Create alarm, delete alarms, 
+class AlarmManager:
+    def __init__(self, alarms):
+        self.alarms = alarms
 
-def get_cpu_percent_usage():# gets CPU percent
-    psutil.cpu_percent
-def get_memory_percent_usage(): 
-    pass
-def get_disk_percent_usage():
-    pass
-def get_all_system_percent_usage():
-    pass
+    def add_alarm(self, alarm_type, alarm_level):
+        self.alarms[alarm_type].append(alarm_level)
+        print(f"\nDitt {alarm_type} alarm är inställt på {alarm_level}%")
+
+    def save_to_file(self):
+        pass
+
+#et_cpu_alarm = AlarmManager()
+#int(input(set_cpu_alarm.add_alarm("cpu", "set alarm: ")))
+#type_of_alarm_input = input("Vilket typ av larm vill du ställa in? ")
+#new_alarm_level_input = input("Ställ in en procentsats mellan 0-100: ")
+
+    
+    
+    
+
--- main2.0/main.py
+++ main2.0/main.py
@@ -1,4 +1,7 @@
+import psutil
 from menu import handle_menu, press_enter_to_continue
+from monitoring import get_cpu_status, get_disk_status, get_memory_status
+from alarms import AlarmManager
 
 alarms_storage = {
     "cpu": [],
@@ -24,6 +27,7 @@ alarm_menu = {
 
 start_up_monitoring = False
 main_menu_is_running = True
+alarm_manager = AlarmManager(alarms_storage)
 while main_menu_is_running:
     menu_choice = handle_menu("HUVUDMENY", main_menu)
 
@@ -31,25 +35,41 @@ while main_menu_is_running:
         start_up_monitoring = True
         print("Övervakning påbörjad")
         press_enter_to_continue()
-    if menu_choice == "2":
-        print("då")
+        
+    elif menu_choice == "2":
+        if start_up_monitoring == True:
+            print(get_cpu_status())
+            print(get_memory_status())
+            print(get_disk_status())
+            press_enter_to_continue()
+        else:
+            print("❌Systemövervakning ej startad❌")
+
+    elif menu_choice == "3":
+        alarm_menu_choice = handle_menu("ALARM MENY", alarm_menu)
+        alarm_level_choice = input("Välj larmnivå mellan 1-100: ") # felhantering/validering
+        if alarm_menu_choice == "1":#CPU menu choice
+            alarm_manager.add_alarm("cpu", alarm_level_choice)
+        elif alarm_menu_choice == "2":# Memory menu choice
+            alarm_manager.add_alarm("memory", alarm_level_choice)
+        elif alarm_menu_choice == "3":# DISK menu choice
+            alarm_manager.add_alarm("disk", alarm_level_choice)
+        elif alarm_menu_choice == "4":# Return to main menu
+            break
         press_enter_to_continue()
-    if menu_choice == "3":
-        print("tjabba")
-        press_enter_to_continue()
-    if menu_choice == "4":
+    elif menu_choice == "4":
         print("44")
         press_enter_to_continue()
-    if menu_choice == "5":
+    elif menu_choice == "5":
         print("55")
         press_enter_to_continue()
-    if menu_choice == "6":
+    elif menu_choice == "6":
         print("66")
         press_enter_to_continue()
-    if menu_choice == "7":
+    elif menu_choice == "7":
         print("77")
         press_enter_to_continue
-    if menu_choice == "8":
+    elif menu_choice == "8":
         print("Avslutar programmet")
         break
 
