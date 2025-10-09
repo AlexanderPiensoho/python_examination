00baf1bc49e72506ae70caec6c37317b2a20d5f6
piensoho@gmail.com <piensoho@gmail.com>
2025-10-09 16:42:05 +0200

    Refactor alarm management and logging functionality; implement JSON storage for alarms and enhance user input handling and added logging

---
+++ main1.0/log.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+'''
+log-testing right now before i put it in the program.
+'''
+def log_event(message):
+    timestamp = datetime.now().strftime("%d/%m/%Y_%H:%M:%S")
+    log_message = f"{timestamp}_{message}"
+    
+    with open("monitoring.log", "a") as log_file:
+        log_file.write(log_message + "\n")
+
+
+#log_event("Här_är_loggfilen")
+
+'''
+print("\n=== Loggar ===")
+with open("monitoring.log", "r") as f:
+    print(f.read())
+    '''
--- main1.0/main.py
+++ main1.0/main.py
@@ -1,63 +1,41 @@
 from monitor import start_monitoring, get_cpu_status, get_disk_status, get_memory_status, is_alarm_monitoring
 from menu import show_main_menu, main_menu_input_choice, show_alarm_menu, alarm_menu_input, press_enter_to_continue
-from alarm import show_all_active_alarms, AlarmManager, alarm_percent_input
-import json
-#import psutil
-#import time
-# Set_alarm funktion, för jag får den inte att fungera i annan fil pga alarms dict ligger här
-def set_alarm(alarm_type, input_message):
-    while True:
-        try:
-            alarm_value = int(input(input_message))
-            if 0<= alarm_value <=100:
-                alarms[alarm_type].append(alarm_value)
-                print(f"\nDitt {alarm_type} alarm är inställt på {alarm_value}%".upper())
-                press_enter_to_continue()
-                break
-            else:
-                print("Siffran måste vara mellan 0-100")
-        except ValueError: 
-            print("Felaktigt värde, det måste vara en siffra mellan 0-100")
+from alarm import show_all_active_alarms, AlarmManager, alarm_percent_input, show_all_alarms_numbered
+from log import log_event
 
-'''pre-defined values beacuse of testing. 
-alarms = {
-    "cpu": [2, 10, 40],
-    "memory": [2,30, 40],
-    "disk": [2]
-}
-'''
-
-set_alarm_menu_is_running = False
-main_menu_is_running = True
-monitoring_active = False
-alarm_monitoring = False
-alarm_manager = AlarmManager()
-saved_alarms = alarm_manager.load_from_file()
-
-
+# Can i get this in a function?
 print("=================================")
 print("====Välkommen till huvudmenyn====".upper())
 print("=================================")
 
+#Main program starts here
 def main():
+    log_event("Användare_startade_programmet")
+    main_menu_is_running = True
+    alarm_monitoring = False
+    alarm_manager = AlarmManager()
+    monitoring_active = False
     while main_menu_is_running:
         show_main_menu()
         menu_choice = main_menu_input_choice()
-
+        #Starts monitoring. No threading, so it only turns monitoring to True.
         if menu_choice == "1":
             monitoring_active = True
             start_monitoring()
+            log_event("Bakgrundövervakning_startad")
             press_enter_to_continue()
-
+        #Show status for CPU, Memory and Disk
         elif menu_choice == "2":
             if monitoring_active:
                 print(get_cpu_status())
                 print(get_memory_status())
                 print(get_disk_status())
+                log_event("System_status_hämtad_lyckades")
                 press_enter_to_continue()
             else:
+                log_event("System_status_hämtad_misslyckades")
                 print("❌Systemövervakning ej startad❌")
-
+        #Shows a submenu with 4 choices. Set alarm 1.2.3 and return main menu 4. 
         elif menu_choice == "3":
             set_alarm_menu_is_running = True
             while set_alarm_menu_is_running:
@@ -65,36 +43,51 @@ def main():
                 alarm_menu_choice = alarm_menu_input()
                 if alarm_menu_choice == "1":
                     alarm_threshold = alarm_percent_input()
-                    alarm_manager.add_alarm("cpu", alarm_threshold)
+                    alarm_manager.add_alarm("cpu", alarm_threshold) #Adds alarm to a separet JSON file
+                    log_event(f"CPU_alarm_satt_på_{alarm_threshold}_%")
 
                 elif alarm_menu_choice == "2":
                     alarm_threshold = alarm_percent_input()
-                    print(f"DEBUG: alarm_threshold = {alarm_threshold}, type = {type(alarm_threshold)}")
                     alarm_manager.add_alarm("memory", alarm_threshold)
+                    log_event(f"memory_alarm_satt_på_{alarm_threshold}_%")
                                 
                 elif alarm_menu_choice == "3":
                     alarm_threshold = alarm_percent_input()
                     alarm_manager.add_alarm("disk", alarm_threshold)
+                    log_event(f"disk_alarm_satt_på_{alarm_threshold}_%")
 
                 elif alarm_menu_choice == "4":
                     set_alarm_menu_is_running = False
                     print("\nDu skickas tillbaka till huvudmenyn".upper())
+                    log_event("Användaren_återvände_till_huvudmenyn")
                     
-
+        #Shows all alarms that is stored within the JSON
         elif menu_choice == "4":
             show_all_active_alarms(alarm_manager.alarms)
+            log_event("Visade_alla_aktiva_alarm")
             press_enter_to_continue()
-            
+        #Starts active monitoring and calls out alarms when triggered.      
         elif menu_choice == "5":
+            log_event("Startade_aktiv_övervakning")
             is_alarm_monitoring(alarm_monitoring, alarm_manager.alarms)
-            
+        #Here you will be able to remove alarms from the JSON 
         elif menu_choice == "6":
-            pass
-
+            alarm_list = show_all_alarms_numbered(alarm_manager.alarms)
+            alarm_remove_choice = int(input("Vilket alarm vill du ta bort?: "))
+            idx = alarm_remove_choice -1
+            alarm_type, threshold = alarm_list[idx]
+            alarm_manager.remove_alarm(alarm_type, threshold)
+            press_enter_to_continue()
+        #Exits program
         elif menu_choice == "7":
+            print("\n=== Loggar ===")
+            with open("monitoring.log", "r") as f:
+                print(f.read())
+        elif menu_choice == "8":
             print("===============================")
             print("======AVSLUTAR PROGRAMMET======".upper())
             print("===============================")
+            log_event("Användare_avslutade_programmet")
             break
 
 
