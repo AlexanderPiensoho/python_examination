280600d8455b81a00e5bc4b8664e97f44fb17f41
piensoho@gmail.com <piensoho@gmail.com>
2025-10-21 15:12:20 +0200

    added docustring, and cleaned up current_log. Added alarm_typ_choices as global varible instead of local within the loop.

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
-from monitor import start_monitoring, print_system_status, active_monitoring
-from menu import validate_input, press_enter_to_continue, show_dynamic_menu, exit_program
+from monitor import start_monitoring, print_system_status, active_monitoring, print_no_monitoring_active
+from menu import validate_input, press_enter_to_continue, show_dynamic_menu, exit_program, leave_alarm_menu
 from alarm import AlarmManager, user_remove_alarm, create_alarm_from_user, show_all_alarms_numbered
 from log import log_event, print_current_log
-from datetime import datetime
+
 
 main_menu ={
     "1": "Starta övervakning",
@@ -21,68 +21,70 @@ alarm_menu = {
     "3": "DISK",
     "4": "Tillbaka till huvudmenyn"
 }
+alarm_type_choices = {
+    1: "cpu",
+    2: "memory",
+    3: "disk"
+    }
 
 
-def main():
-    current_log = datetime.now().strftime("log_start_date_%d_%m_%Y_time_%H-%M-%S.log")
-    log_event("Användare_startade_programmet", current_log)
+def main() -> None:
+    '''
+    This program can get system statuses, set/delete/show alarms
+    and monitor the alarms against the cumputers current thresholds
+    is logs everything that happens per program-run aswell
+    '''
+    log_event("Användare_startade_programmet")
     main_menu_is_running = True
     monitoring_active = False
     alarm_manager = AlarmManager()
     while main_menu_is_running:
         show_dynamic_menu("HUVUDMENY", main_menu)
-        main_choice = validate_input(1, 8)
+        main_choice = validate_input("Gör ett menyval mellan", 1, len(main_menu))
 
         match main_choice:
             case 1:
                 monitoring_active = start_monitoring()
-                log_event("Bakgrundövervakning_startad", current_log)
-                press_enter_to_continue()
 
             case 2:
-                #Checks if monitoring is started before listing system status
-                if monitoring_active:
-                    print_system_status(current_log)
+                if not monitoring_active:
+                    print_no_monitoring_active()
                 else:
-                    log_event("System_status_hämtad_misslyckades", current_log)
-                    print("❌Systemövervakning ej startad❌")
-                    press_enter_to_continue()
+                    print_system_status()
 
             case 3:
                 alarm_menu_running = True
                 while alarm_menu_running:
-                    show_dynamic_menu("ALARM MENY", alarm_menu)
-                    alarm_choice = validate_input(1, 4)
+                    show_dynamic_menu("LARM MENY", alarm_menu)
+                    alarm_choice = validate_input("Gör ett menyval mellan", 1, len(alarm_menu))
                     match alarm_choice:
                         case 1 | 2 | 3:
-                            alarm_type = {1: "cpu", 2: "memory", 3: "disk"}
-                            create_alarm_from_user(alarm_manager, alarm_type[alarm_choice], current_log)
+                            create_alarm_from_user(alarm_manager, alarm_type_choices[alarm_choice])
 
                         case 4: 
-                            alarm_menu_running = False
-                            print("\nDu skickas tillbaka till huvudmenyn".upper())
-                            log_event("Användaren_återvände_till_huvudmenyn", current_log)
+                            alarm_menu_running = leave_alarm_menu()
 
             case 4:
                 show_all_alarms_numbered(alarm_manager.get_alarm())
-                log_event("Visade_alla_aktiva_alarm", current_log)
                 press_enter_to_continue()
     
-            case 5:
-                #Enter active monitoring mode that check current status against the set alarms, only trigger the highest.
-                log_event("Startade_aktiv_övervakning", current_log)
-                active_monitoring(alarm_manager.alarms, monitoring_active, current_log)
+            case 5: 
+                if not monitoring_active:
+                    print_no_monitoring_active()
+                else:
+                    active_monitoring(alarm_manager.get_alarm(), monitoring_active)
+                    
 
             case 6:
                 alarm_list = show_all_alarms_numbered(alarm_manager.get_alarm())
-                user_remove_alarm(alarm_list, alarm_manager, current_log)
+                user_remove_alarm(alarm_list, alarm_manager)
                 
             case 7:
-                print_current_log(current_log)
+                print_current_log()
                 press_enter_to_continue()
 
             case 8:
-                main_menu_is_running = exit_program(log_event, current_log)
+                main_menu_is_running = exit_program()
 
 if __name__ == "__main__":
     main()
